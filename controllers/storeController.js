4a18c677b560db2a4d1d0abd7587eed1bdd5def5
Spencer Snyder <sasnyde2@gmail.com>
2017-09-22 23:41:41 -0400

    store page added, photo uploads

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,4 +1,34 @@
 const Store = require('../models/store');
+const multer = require('multer');
+const jimp = require('jimp');
+const uuid = require('uuid');
+
+const multerOptions = {
+  storage: multer.memoryStorage(),
+  fileFilter(req, file, next) {
+    const isPhoto = file.mimetype.startsWith('image/');
+    if (isPhoto) {
+      next(null, true);
+    } else {
+      next({ message: 'That file type is not allowed' }, false);
+    }
+  }
+};
+
+exports.upload = multer(multerOptions).single('photo');
+
+exports.resize = async (req, res, next) => {
+  if (!req.file) {
+    next();
+    return;
+  }
+  const extension = req.file.mimetype.split('/')[1];
+  req.body.photo = `${uuid.v4()}.${extension}`;
+  const photo = await jimp.read(req.file.buffer);
+  await photo.resize(800, jimp.AUTO);
+  await photo.write(`./public/uploads/${req.body.photo}`);
+  next();
+};
 
 exports.homePage = (req, res) => {
   res.render('index');
@@ -9,8 +39,8 @@ exports.addStore = (req, res) => {
 };
 
 exports.createStore = async (req, res) => {
-  const { name, description, tags } = req.body;
-  const store = new Store({ name, description, tags });
+  req.body.location.type = 'Point';
+  const store = new Store(req.body);
   const savedStore = await store.save();
   req.flash(
     'success',
@@ -34,6 +64,7 @@ exports.editStore = async (req, res) => {
 };
 
 exports.updateStore = async (req, res) => {
+  req.body.location.type = 'Point';
   const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
     new: true,
     runValidators: true
@@ -45,4 +76,10 @@ exports.updateStore = async (req, res) => {
   );
 
   res.redirect(`/stores/${store._id}/edit`);
+};
+
+exports.getStoreBySlug = async (req, res) => {
+  const store = await Store.findOne({ slug: req.params.slug });
+  if (!store) return next();
+  res.render('store', { title: store.name, store });
 };
