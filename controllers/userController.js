0e6241a22d2ebf6f31f938a654b79fcf35a928a0
Spencer Snyder <sasnyde2@gmail.com>
2017-09-24 14:35:48 -0400

    login/logout functionality

---
+++ controllers/userController.js
@@ -0,0 +1,48 @@
+const User = require('../models/User');
+const promisify = require('es6-promisify');
+
+exports.loginForm = (req, res) => {
+  res.render('login');
+};
+
+exports.registerForm = (req, res) => {
+  res.render('register');
+};
+
+exports.validateRegister = (req, res, next) => {
+  req.sanitizeBody('name');
+  req.checkBody('name', 'You must supply a name').notEmpty();
+  req.checkBody('email', 'You must supply a email').isEmail();
+  req.sanitizeBody('email').normalizeEmail({
+    remove_dots: false,
+    remove_extension: false,
+    gmail_remove_subaddress: false
+  });
+  req.checkBody('password', 'Password cannot be blank').notEmpty();
+  req
+    .checkBody('password-confirm', 'Password confirm cannot be blank')
+    .notEmpty();
+  req
+    .checkBody('password-confirm', 'Passwords do not match')
+    .equals(req.body.password);
+
+  const errors = req.validationErrors();
+  if (errors) {
+    req.flash('error', errors.map(err => err.msg));
+    res.render('register', {
+      title: 'Register',
+      body: req.body,
+      flashes: req.flash()
+    });
+    return;
+  }
+  next();
+};
+
+exports.register = async (req, res, next) => {
+  const { email, name } = req.body;
+  const user = new User({ email, name });
+  const register = promisify(User.register, User);
+  const regUser = await register(user, req.body.password);
+  next();
+};
---
+++ models/User.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const md5 = require('md5');
+const validator = require('validator');
+const passportLocalMongoose = require('passport-local-mongoose');
+const mongodbErrorHandler = require('mongoose-mongodb-errors');
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    unique: true,
+    lowercase: true,
+    trim: true,
+    validate: [validator.isEmail, 'Invalid e-mail address'],
+    require: 'Please supply an e-mail address'
+  },
+  name: {
+    type: String,
+    required: 'Please supply a name',
+    trim: true
+  }
+});
+
+userSchema.virtual('gravatar').get(function() {
+  const hash = md5(this.email);
+  return `https://gravatar.com/avatar/${hash}?s=200`;
+});
+
+userSchema.plugin(passportLocalMongoose, { usernameField: 'email' });
+userSchema.plugin(mongodbErrorHandler);
+
+module.exports = mongoose.model('User', userSchema);
