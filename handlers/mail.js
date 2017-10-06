d7f9824b15a6b2bc31941afc58ef00400aca322f
Spencer Snyder <sasnyde2@gmail.com>
2017-10-05 21:40:45 -0400

    catch up

---
+++ handlers/mail.js
@@ -0,0 +1,20 @@
+const nodemailer = require('nodemailer');
+const pug = require('pug');
+const juice = require('juice');
+const htmlToText = require('html-to-text');
+const promisify = require('es6-promisify');
+
+const transport = nodemailer.createTransport({
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
+  auth: {
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS
+  }
+});
+
+exports.send = async options => {
+  const mailOptions = {
+    from: `Spencer Snyder`
+  };
+};
